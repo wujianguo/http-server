70ab4a241bc800e0719b9b0298df2483c44a3fc6
wujianguo <github@wujianguo.org>
2016-01-01 12:53:21 +0800

    add uv

--- http-server/main.c
+++ http-server/main.c
@@ -7,9 +7,11 @@
 //
 
 #include <stdio.h>
+#include "uv.h"
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
+    uv_run(uv_default_loop(), UV_RUN_DEFAULT);
     return 0;
 }
